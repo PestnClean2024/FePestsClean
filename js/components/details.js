0a860fdc0af1303c8e6c488001653e33934516d4
nguyenanh1997dz <nguyenanh1997dz@gmail.com>
2024-07-25 12:31:19 +0700

    Nothing

---
+++ js/components/details.js
@@ -0,0 +1,35 @@
+const quantityInput = document.getElementById("quantity");
+const decreaseButton = document.getElementById("decrease");
+const increaseButton = document.getElementById("increase");
+let quantity = parseInt(quantityInput.value, 10);
+
+decreaseButton.addEventListener("click", () => {
+  if (quantity > 1) {
+    quantity--;
+    quantityInput.value = quantity;
+  }
+});
+
+increaseButton.addEventListener("click", () => {
+  quantity++;
+  quantityInput.value = quantity;
+});
+
+
+$(document).ready(function () {
+  $(".slice-carousel").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    dots: false,
+    infinite: true,
+    draggable: true,
+    swipeToSlide: true,
+    centerMode: false,
+    centerPadding: "0px",
+  });
+
+  $(".thumb").on("click", function () {
+    var src = $(this).attr("src");
+    $("#mainImage").attr("src", src);
+  });
+});
