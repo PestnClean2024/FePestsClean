aa8311dc3127c305a5853b635c8dec5e8a1cc9f5
LuuKiett <luukie0401@gmail.com>
2024-07-26 14:24:59 +0700

    fix footer

--- resources/views/client/layouts/footer.php
+++ resources/views/client/layouts/footer.php
@@ -1,22 +1,22 @@
 <footer>
     <div class="footer">
       <div class="footer-log-re">
-        <p class="footer-p-register">Đăng ký ngay để cập nhật tin tức</p>
-        <p class="footer-p-Sign">Sign up for deals, new products and promotions</p>
-        <i class="fa-regular fa-envelope"></i> <input type="text" name="" id="" placeholder="Nhập Email của bạn"> <a class="footer-a" href="#">Đăng ký ngay</a>
-        <hr class="input-hr">
+        <p class="footer-p-register text-uppercase">Đăng ký ngay để cập nhật tin tức</p>
+        <form id="collect-info">
+          <i class="fa-regular fa-envelope"></i> <input type="text" name="" id="" placeholder="Nhập Email của bạn"> <a class="footer-a" href="#">Đăng ký ngay</a>
+        </form>
       </div>
     </div>
 
     <div class="footer-div">
       <div class="footer-5 width-img">
-        <img src="/FePestsClean/resources/images/Footer/Logo.png" alt="" class="footer-img">
+        <img src="/FePestsClean/resources/images/Footer/Logo.png" alt="" class="footer-img mb-4">
         <p class="footer-p"><span class="icon"><i class="fa-sharp fa-solid fa-location-dot"></i></span> Lầu 8, 123 Lý Chính Thắng, Phường Võ Thị Sáu, Quận 3, Thành phố Hồ Chí Minh</p>
         <p class="footer-p"><span class="icon"><i class="fa-solid fa-phone"></i></span> 0968 36 36 00</p>
         <p class="footer-p"><span class="icon"><i class="fa-solid fa-envelope"></i></span> sale@petnclean.vn | sale@petnclean.vn</p>
         <p class="footer-p icon">
           <i class="fa-brands fa-facebook"></i>
-          <i class="fa-brands fa-instagram"></i>
+          <i class="fa-brands fa-instagram mx-2"></i>
           <i class="fa-brands fa-youtube"></i>
         </p>
       </div>
@@ -48,14 +48,16 @@
         <p class="footer-text-p font">Khác</p>
         <p class="footer-text-p">Hỏi đáp</p>
         <p class="footer-text-p">Liên hệ</p>
-        <img src="img/footer-dathongtin.png" alt="">
+        <img src="/FePestsClean/resources/images/Footer/footer-dathongtin.png" alt="">
       </div>
     </div>
     <hr>
 
     <div class="footer-a">
       <a href="#">Copyright © 2024 Petnclean</a> |
-      <a href="#">Công ty TNHH Thương Mại Dịch Vụ Xuất Khẩu Nguyên Duy Privacy Policy</a> |
+      <a href="#">Công ty TNHH Thương Mại Dịch Vụ Xuất Khẩu Nguyễn Duy </a>
+      <span class="mx-2">|</span>
+      <a href="#" class="me-2">Privacy Policy</a>
       <a href="#">Terms & Conditions</a>
     </div>
   </footer>
