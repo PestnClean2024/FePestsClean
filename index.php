e005f12e7387d90b45460afb2b25c6ea93f24313
Duong <duongsulam3@gmail.com>
2024-07-27 15:14:53 +0700

    Update, fix bug HomePage

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-    // include 'resources/views/client/index.php';
-    include 'resources/views/client/service.php';
+    include 'resources/views/client/index.php';
+    // include 'resources/views/client/service.php';
 ?>
