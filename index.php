784ec871e97d286e77d048213056c69cc0600693
Duong <duongsulam3@gmail.com>
2024-07-28 17:43:04 +0700

    Update HomePage

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    // include 'resources/views/client/index.php';
+    include 'resources/views/client/index.php';
     // include 'resources/views/client/service.php';
-    include 'resources/views/client/blog.php';
+    // include 'resources/views/client/blog.php';
 ?>
