4563e4b7e0faf77ac63fe7bc7b07fd26c545f4e1
Duong <duongsulam3@gmail.com>
2024-07-29 12:54:09 +0700

    Update Features Homepage

---
+++ resources/views/client/banner-carousel.php
@@ -0,0 +1,20 @@
+<section class="service-ads-banner">
+    <div id="bannerCarousel" class="carousel slide" data-bs-interval="5000">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="../../images/serviceCleaning/services-banner.png" class="d-block w-100 img-fluid" alt="">
+            </div>
+            <div class="carousel-item">
+                <img src="../../images/serviceCleaning/services-banner.png" class="d-block w-100 img-fluid" alt="">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#bannerCarousel" data-bs-slide="prev">
+            <img src="../../images/serviceCleaning/carousel-arrow-left.svg" class="img-fluid">
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#bannerCarousel" data-bs-slide="next">
+            <img src="../../images/serviceCleaning/carousel-arrow-right.svg" class="img-fluid">
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+</section>
