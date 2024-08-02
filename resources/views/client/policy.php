07473618747eaad84603e99c91e9ffbaa6211c3d
Duong <duongsulam3@gmail.com>
2024-08-02 15:46:35 +0700

    Update Policy Page - CSBH Tab

--- resources/views/client/policy.php
+++ resources/views/client/policy.php
@@ -166,119 +166,117 @@
                     <h1>Chính Sách Bảo Hành - Đổi trả</h1>
                     <img class="csbmtt-tab-content-header-img" src="../../../../FePestsClean/resources/images/policy/policy-tab-banner2.png">
                     <div class="csbmtt-tab-policy-content">
-                        <div class="csbmtt-text-container">
+                        <!-- <div class="csbmtt-text-container">
                             <p class="csbmtt-tab-policy-content-headline">
                                 CHÍNH SÁCH BẢO MẬT THÔNG TIN KHÁCH HÀNG:
                             </p>
                             <p class="csbmtt-tab-policy-content-support">
                                 Chào mừng bạn đến với Công Ty TNHH TM DV XNK NGUYỄN DUY (PestnClean)! Chúng tôi rất trân trọng sự tin tưởng của bạn và cam kết bảo vệ thông tin cá nhân của bạn. Chính sách bảo mật này mô tả cách chúng tôi thu thập, sử dụng và bảo vệ thông tin cá nhân của bạn khi bạn sử dụng trang web của chúng tôi và các quy định của pháp luật Việt Nam. Cụ thể:
                             </p>
-                        </div>
+                        </div> -->
+                        <p class="csbh-tab-normal-text">
+                            Chào mừng bạn đến với Công Ty TNHH TM DV XNK NGUYỄN DUY (PestnClean).
+                            Chúng tôi cam kết cung cấp các sản phẩm chất lượng và dịch vụ tốt nhất cho khách hàng của chúng tôi. Chính sách bảo hành đổi trả này mô tả các điều kiện và điều khoản áp dụng cho việc bảo hành, đổi trả sản phẩm của bạn khi mua hàng từ chúng tôi.
+                        </p>
+                        <p class="csbh-tab-normal-text">
+                            Để đảm bảo quyền lợi của Quý khách trong việc đổi trả hàng và bảo hành sản phẩm, vui lòng kiểm tra hàng hoá, đối chiếu với chứng từ và phiếu bảo hành (nếu có) trước khi nhận hàng. Ngoài ra, vui lòng liên hệ với bộ phận bảo hành để biết thêm thông tin chi tiết về quy định bảo hành của sản phẩm:
+                        <ul>
+                            <li>
+                                Email: info<u>@pestnclean.vn</u>
+                            </li>
+                            <li>
+                                Hotline: 0868 36 36 00
+                            </li>
+                        </ul>
+                        </p>
                     </div>
                     <div class="csbmtt-tab-policy-detail-content">
                         <div class="csbmtt-detail-text-container">
                             <p class="csbmtt-detail-text-title">
-                                1. Thông Tin Thu Thập
+                                1. Trường Hợp Được Bảo Hành
                             </p>
                             <div class="text-and-ul-container">
-                                <p class="csbmtt-detail-text-normal">
-                                    Khi bạn tạo tài khoản hoặc mua hàng trên trang web của chúng tôi, chúng tôi có thể thu thập các thông tin cá nhân sau đây:
                                 <ul>
-                                    <li>Tên của bạn</li>
-                                    <li>Địa chỉ email</li>
-                                    <li>Địa chỉ giao hàng</li>
-                                    <li>Thông tin thanh toán</li>
+                                    <li class="csbh-tab-normal-text">Nếu sự cố được xác định là do nhà sản xuất gây ra và vẫn còn thời hạn bảo hành</li>
+                                    <li class="csbh-tab-normal-text">Không có dấu hiệu can thiệp của bên thứ 03 (sửa chữa ngoài) trên sản phẩm.</li>
+                                    <li class="csbh-tab-normal-text">Số sản phẩm, tem niêm phong và phiếu bảo hành của sản phẩm phải chính xác, không bị mất hoặc rách.</li>
+                                    <li class="csbh-tab-normal-text">Hàng hóa không bị ảnh hưởng bởi các yếu tố môi trường như nước, hóa chất ăn mòn và nhiệt gây biến dạng.</li>
                                 </ul>
-                                Chúng tôi cũng có thể tự động thu thập một số thông tin không cá nhân về việc bạn sử dụng trang web của chúng tôi, bao gồm địa chỉ IP, loại trình duyệt, thời gian truy cập và trang web bạn đã truy cập trước khi đến với chúng tôi.
-                                </p>
                             </div>
                         </div>
                         <div class="csbmtt-detail-text-container">
                             <p class="csbmtt-detail-text-title">
-                                2. Sử Dụng Thông Tin
+                                2. Trường Hợp Không Được Bảo Hành
                             </p>
                             <div class="text-and-ul-container">
-                                <p class="csbmtt-detail-text-normal">
-                                    Chúng tôi sử dụng thông tin mà bạn cung cấp để:
                                 <ul>
-                                    <li>Cung cấp dịch vụ cho bạn và xử lý đơn hàng của bạn</li>
-                                    <li>Gửi thông tin về sản phẩm, khuyến mãi và sự kiện</li>
-                                    <li>Cải thiện và nâng cấp website của chúng tôi</li>
+                                    <li>Những sản phẩm, mặt hàng không thể xác định được nguồn gốc. PestnClean có quyền từ chối bảo hành.</li>
+                                    <li>Phiếu bảo hành của sản phẩm không còn hoặc đã hết hạn.</li>
+                                    <li>Phiếu bảo hành bị rách, không còn, bị dán đè hoặc bị sửa đổi</li>
+                                    <li>Phiếu bảo hành không bao gồm số serial và ngày xảy ra sự kiện.</li>
+                                    <li>Số serial trên máy và phiếu bảo hành không trùng khớp nhau hoặc không thể tìm thấy.</li>
+                                    <li>Sản phẩm bị hư hỏng do các tác động cơ học như rơi, vỡ, va đập, trầy xước, móp méo, ẩm ướt, hoen rỉ hoặc do hỏa hoạn, thiên tai</li>
+                                    <li>Sản phẩm có dấu hiệu hư hỏng do côn trùng hoặc chuột bọ gây ra.</li>
+                                    <li>Sản phẩm bị hư hỏng do sử dụng sách hướng dẫn không đúng hoặc sử dụng điện áp không phù hợp với quy định.</li>
+                                    <li>Các phụ kiện kèm theo, chẳng hạn như điều khiển từ xa, pin điều khiển, dây nguồn, dây tín hiệu, đèn tín hiệu, tai nghe, quạt trên thiết bị, có thể bị hỏng và gây cháy.</li>
+                                    <li>Tự ý tháo dỡ, thay đổi cấu trúc sản phẩm, sử dụng hướng dẫn sai hoặc sử dụng linh kiện không phù hợp.</li>
+                                    <li>Nếu các sản phẩm được mua tại PestnClean quá hạn bảo hành, Quý khách có thể được công ty chúng tôi sửa chữa với chi phí khác.</li>
                                 </ul>
-                                Chúng tôi cam kết không bán, chia sẻ hoặc trao đổi thông tin cá nhân của bạn với bên thứ ba mà không có sự đồng ý của bạn, trừ khi cần thiết để cung cấp dịch vụ cho bạn hoặc tuân thủ theo yêu cầu pháp luật.
-                                </p>
                             </div>
                         </div>
                         <div class="csbmtt-detail-text-container">
                             <p class="csbmtt-detail-text-title">
-                                3. Bảo Mật Thông Tin
+                                3. Quy Trình Bảo Hành
                             </p>
                             <div class="text-and-ul-container">
                                 <p class="csbmtt-detail-text-normal">
-                                    Chúng tôi cam kết bảo vệ thông tin cá nhân của bạn và đã triển khai các biện pháp an ninh phù hợp để bảo vệ thông tin đó khỏi lạm dụng, mất mát hoặc truy cập trái phép. Tuy nhiên, không có phương pháp truyền tải thông tin trên Internet hoặc lưu trữ điện tử nào là 100% an toàn.
-                                </p>
-                            </div>
-                        </div>
-                        <div class="csbmtt-detail-text-container">
-                            <p class="csbmtt-detail-text-title">
-                                4. Quyền Lợi của Bạn
-                            </p>
-                            <div class="text-and-ul-container">
-                                <p class="csbmtt-detail-text-normal">
-                                    Bạn có quyền truy cập, sửa đổi hoặc xóa bất kỳ thông tin cá nhân nào chúng tôi đã thu thập về bạn. Bạn cũng có quyền phản đối việc sử dụng thông tin của bạn cho mục đích tiếp thị hoặc liên hệ trực tiếp với chúng tôi để biết thêm thông tin.
-                                </p>
-                            </div>
-                        </div>
-                        <div class="csbmtt-detail-text-container">
-                            <p class="csbmtt-detail-text-title">
-                                5. Liên Hệ Chúng Tôi
-                            </p>
-                            <div class="text-and-ul-container">
-                                <p class="csbmtt-detail-text-normal">
-                                    Nếu bạn có bất kỳ câu hỏi hoặc ý kiến nào về Chính Sách Bảo Mật của chúng tôi, vui lòng liên hệ với chúng tôi
+                                    Vui lòng liên hệ với PestnClean qua các phương tiện được cung cấp trên trang web của chúng tôi nếu bạn đang gặp bất kỳ lỗi hỏng hóc nào nằm trong Trường Hợp Được Bảo Hành sản phẩm.
                                 </p>
                                 <ul>
-                                    <li>Trụ sở chính: Lầu 8, 123 Lý Chính Thắng, P. Võ Thị Sáu, Quận 3, Tp. HCM, Viet Nam.</li>
-                                    <li>Email: info@pestnclean.vn</li>
-                                    <li>Hotline: 0868 36 36 00.</li>
-                                    <li>Cảm ơn bạn đã tin tưởng và sử dụng dịch vụ của PestnClean!</li>
+                                    <li>Để thực hiện quy trình bảo hành, bạn cần cung cấp hóa đơn hoặc biên nhận để chứng minh nguồn hàng từ PestnClean, giấy bảo hành liên quan đến sản phẩm.</li>
+                                    <li>Sau khi kiểm tra và xác nhận rằng sản phẩm thuộc Trường Hợp Bảo Hành, chúng tôi sẽ tiến hành theo chính sách chúng tôi đã cung cấp.</li>
                                 </ul>
-                                <p class="csbmtt-detail-text-normal">
-                                    Cảm ơn bạn đã tin tưởng và sử dụng dịch vụ của PestnClean!
-                                </p>
                             </div>
                         </div>
                         <div class="csbmtt-detail-text-container">
                             <p class="csbmtt-detail-text-title">
-                                6. Những người hoặc tổ chức có thể được tiếp cận với thông tin đó
+                                4. Trường Hợp Được Đổi Trả
                             </p>
                             <div class="text-and-ul-container">
                                 <ul>
-                                    <li>
-                                        <p class="csbmtt-detail-text-normal">
-                                            Các đối tác là bên cung cấp dịch vụ cho chúng tôi liên quan đến thực hiện đơn hàng và chỉ giới hạn trong phạm vi thông tin cần thiết cũng như áp dụng các quy định đảm bảo an ninh, bảo mật các thông tin cá nhân.
-                                        </p>
-                                    </li>
-                                    <li>
-                                        <p class="csbmtt-detail-text-normal">
-                                            Chúng tôi sử dụng dịch vụ từ một nhà cung cấp dịch vụ là bên thứ ba để thực hiện một số hoạt động liên quan đến website amall.vn. Khi đó, bên thứ ba có thể truy cập hoặc xử lý các thông tin cá nhân trong quá trình cung cấp các dịch vụ đó. Chúng tôi yêu cầu các bên thứ ba này tuân thủ mọi luật lệ về bảo vệ thông tin cá nhân liên quan và các yêu cầu về an ninh liên quan đến thông tin cá nhân.
-                                        </p>
-                                    </li>
-                                    <li>
-                                        <p class="csbmtt-detail-text-normal">
-                                            Các chương trình có tính liên kết, đồng thực hiện, thuê ngoài cho các mục đích được nêu tại Mục 1 và luôn áp dụng các yêu cầu bảo mật thông tin cá nhân.
-                                        </p>
-                                    </li>
-                                    <li>
-                                        <p class="csbmtt-detail-text-normal">
-                                            Yêu cầu pháp lý: Chúng tôi có thể tiết lộ các thông tin cá nhân nếu điều đó do luật pháp yêu cầu và việc tiết lộ như vậy là cần thiết một cách hợp lý để tuân thủ các quy trình pháp lý.
-                                        </p>
-                                    </li>
-                                    <li>
-                                        <p class="csbmtt-detail-text-normal">
-                                            Chuyển giao kinh doanh (nếu có): trong trường hợp sáp nhập, hợp nhất toàn bộ hoặc một phần với công ty khác, người mua sẽ có quyền truy cập thông tin được chúng tôi lưu trữ, duy trì trong đó bao gồm cả thông tin cá nhân.
-                                        </p>
-                                    </li>
+                                    <li>Sản phẩm cần phải ở trong tình trạng ban đầu và không bị hỏng hoặc sử dụng.</li>
+                                    <li>Một số sản phẩm có thể không được đổi trả hoặc có các điều kiện đặc biệt, vui lòng kiểm tra chi tiết trên trang web của chúng tôi hoặc liên hệ với PestnClean để biết thêm thông tin chi tiết.</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="csbmtt-detail-text-container">
+                            <p class="csbmtt-detail-text-title">
+                                5. Quy Trình Đổi Trả
+                            </p>
+                            <div class="text-and-ul-container">
+                                <ul>
+                                    <li>Nếu bạn phát hiện bất kỳ lỗi hoặc hỏng hóc nào trong sản phẩm, vui lòng liên hệ với chúng tôi qua phương tiện liên lạc được cung cấp trên trang web của chúng tôi.</li>
+                                    <li>Để thực hiện quy trình đổi trả, bạn có thể cần cung cấp chứng minh mua hàng như hóa đơn hoặc biên nhận.</li>
+                                    <li>Sau khi kiểm tra và xác nhận lỗi của sản phẩm, chúng tôi sẽ tiến hành đổi trả hoặc sửa chữa theo quyết định của chúng tôi và trong phạm vi của chính sách bảo hành của chúng tôi.</li>
+                                    <p>Cảm ơn Quý khách đã tin tưởng sử dụng dịch vụ của PestnClean!</p>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="csbmtt-detail-text-container">
+                            <p class="csbmtt-detail-text-title">
+                                6. Hoàn trả và hoàn tiền
+                            </p>
+                            <div class="text-and-ul-container">
+                                <p class="highlight-bold-text">Điều Kiện Hoàn Trả:</p>
+                                <ul>
+                                    <li>Sản phẩm phải còn trong tình trạng ban đầu và không có dấu hiệu đã được sử dụng hoặc cải tạo.</li>
+                                    <li>Yêu cầu hoàn trả phải được thực hiện trong vòng 14 ngày kể từ ngày mua hàng.</li>
+                                    <li>Khách hàng cần cung cấp hóa đơn mua hàng, và mô tả rõ lý do hoàn trả.</li>
+                                </ul>
+                                <p class="highlight-bold-text mt-4">Quy Trình Hoàn Tiền:</p>
+                                <ul>
+                                    <li>Sau khi nhận được yêu cầu và xác minh thông tin, PestnClean sẽ tiến hành kiểm tra và đánh giá điều kiện hoàn trả của sản phẩm.</li>
+                                    <li>Nếu đủ điều kiện, chúng tôi sẽ thực hiện hoàn tiền qua phương thức thanh toán ban đầu của khách hàng trong vòng 7 ngày làm việc.</li>
                                 </ul>
                             </div>
                         </div>
